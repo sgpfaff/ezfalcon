e68eff4e653453df477b374e166b40def1c77d70
Gabriel <gabriel.pfaffman@gmail.com>
2026-04-15 15:14:43 -0400

    Wrap galpy bridge imports in try-except block to handle ImportError gracefully

--- ezfalcon/util/__init__.py
+++ ezfalcon/util/__init__.py
@@ -1,2 +1,5 @@
 from .units import *
-from ._galpy_bridge import _galpy_pot_to_acc_fn, _galpy_pot_to_pot_fn
+try:
+    from ._galpy_bridge import _galpy_pot_to_acc_fn, _galpy_pot_to_pot_fn
+except ImportError:
+    pass
