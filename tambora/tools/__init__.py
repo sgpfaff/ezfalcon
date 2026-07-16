cd5fbd0e61dc44669d7e3bf64ca7a058e89202e7
Gabriel <gabriel.pfaffman@gmail.com>
2026-07-16 02:41:49 -0400

    Remove depreciatd import from tools module.

--- tambora/tools/__init__.py
+++ tambora/tools/__init__.py
@@ -3,6 +3,4 @@
 # rather than silently dropping them when galpy is missing.
 from .galpy_tools import galpydfsampler, galpysampler, galpy_orbit_to_tambora, mkKing_galpy, mkNFW_galpy, mkPlummer_galpy
 
-from .satellite_tools import compute_bound, compute_tidal_radius
-
 from .util import *
